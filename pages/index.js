1890669f6f45f2ce2138a09a8f5d40591603189a
silvadoug <douglasrag2015@gmail.com>
2021-01-31 15:48:46 -0300

    Modificando pagina inicial e de quiz, criando os componentes de Button, Input e QuizContainer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -9,6 +8,9 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 // const BackgroungImage = styled.div`
 //   background-image: url(${db.bg});
@@ -16,17 +18,6 @@ import QuizLogo from '../src/components/QuizLogo';
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px){
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -49,23 +40,22 @@ export default function Home() {
               router.push(`/quiz?name${name}`);
             }}
             >
-              <input
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                placeholder=""
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Digite seu nome"
+                name="nomeDoUsuario"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                { name }
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-            <p>Teste</p>
+            {/* <p>Teste</p> */}
           </Widget.Content>
         </Widget>
         <Footer />
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-export default function QuizPage() {
+import db from '../db.json';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
+
+function LoadingWidget() {
   return (
-    <div>
-      Pagina do quiz
-    </div>
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+      <Widget.Content>
+        Desafio do loading
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  totalQuestions,
+  questionIndex,
+  onSubmit,
+}) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative_${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                  // style={{ display: 'none' }}
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+QuestionWidget.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  question: PropTypes.object.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  questionIndex: PropTypes.number.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            totalQuestions={totalQuestions}
+            questionIndex={questionIndex}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+        {screenState === screenStates.RESULT && <div>Noice</div>}
+      </QuizContainer>
+    </QuizBackground>
   );
 }
